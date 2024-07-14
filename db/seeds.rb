30bed3021012402f30be48ff77f8335700cff9a7
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 18:02:14 -0300

    chore: generating users and contacts with seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Starting seeding, please wait...'
+
+# Limpar dados antigos
+Contact.delete_all
+User.delete_all
+
+# Função para gerar CPF válido
+def generate_valid_cpf
+  CPF.generate
+end
+
+def create_10_contacts(user)
+  10.times do
+    user.contacts.create!(
+      name: Faker::Name.name,
+      cpf: generate_valid_cpf,
+      phone: Faker::PhoneNumber.phone_number,
+      # address: Faker::Address.street_address,
+      address: 'Pasteur',
+      # zip_code: Faker::Address.zip_code,
+      zip_code: '80250-104',
+      complement: Faker::Address.secondary_address
+    )
+  end
+end
+
+# Criar usuário
+user1 = User.create!(
+  email: 'admin@admin.com',
+  password: 'admin'
+)
+
+user2 = User.create!(
+  email: 'admin2@admin.com',
+  password: 'admin2'
+)
+
+create_10_contacts(user1)
+create_10_contacts(user2)
+
+puts 'Seeding completed successfully! A brand new user has been created with 10 contacts!'
