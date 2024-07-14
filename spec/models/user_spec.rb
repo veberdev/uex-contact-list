65a5c415145f88f4be7881eeaf5cd36a550d2112
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 11:48:24 -0300

    test: first user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # tests with factories:
+  let(:user) { build(:user) }
+
+  it 'is a valid user' do
+    expect(user).to be_valid
+  end
 end
