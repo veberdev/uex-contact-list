2602700064c428282080fffcb176c110c95526b8
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 12:01:52 -0300

    fix: error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: 'Invalid email.' }
+                    format: { with: /\A[\w.+-]+@\w+\.\w+\z/, message: 'invalid format.' }
 end
