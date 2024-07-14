dd828ad9e7ac50981d6cce7bcd1ce50e54db810a
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 18:06:11 -0300

    feat: use google's API for geolocalization

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,7 @@
 Geocoder.configure(
-  # api_key: ENV['GOOGLE_MAPS_API_KEY'],
-  # use_https: true,       
-  # lookup: :google,        
-  # timeout: 20,
-  # units: :km
+  api_key: ENV['GOOGLE_MAPS_API_KEY'],
+  use_https: true,       
+  lookup: :google,        
+  timeout: 20,
+  units: :km
 )
