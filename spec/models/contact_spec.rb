a68cb7ba39be84c941a02e8b3f15b724ddf3226a
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 17:18:13 -0300

    tests: add some tests for model Contact

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations and relationships' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:cpf) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:zip_code) }
+    it { is_expected.to belong_to(:user) }
+  end
 end
