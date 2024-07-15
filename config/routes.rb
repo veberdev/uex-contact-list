08bb3d21f79dc2b1de15a8123600c76f7bf9a56f
Weslley Lucas Silva do Santos <weslusisa@gmail.com>
2024-07-14 23:02:05 -0300

    feat: contacts are searcheable by cpf or name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contacts
+      resources :contacts do
+        collection do
+          get 'search', to: 'contacts#search', as: 'search_contacts'
+        end
+      end
       resources :accounts, only: [:index, :create, :destroy]
       resources :sessions, only: :create
       get 'addresses/:cep', to: 'addresses#search_by_cep'
